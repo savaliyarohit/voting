6b290c3261bc68ede1d76a543511495d228c028b
savaliyarohit <savaliyarohit333@gmail.com>
2022-12-21 18:03:26 +0000

    voting

---
+++ age1.java
@@ -0,0 +1,17 @@
+package voting;
+
+public class age1 {
+    public static void main (String []ergs){
+        int a = 18;
+        if(a>=18){
+
+            System.out.println("a is Eligible for votes");
+
+             }else{
+            System.out.println("18 is smaller Not Eligible for votes");
+        }
+
+
+
+    }
+}
---
+++ age2.java
@@ -0,0 +1,14 @@
+package voting;
+
+public class age2 {
+    public static void main(String [] ergs){
+    int b = 18;
+    if(b<18) {
+        System.out.println("18 is Eligible for votes");
+    }else {
+        System.out.println("b is smaller Not Eligible for votes");
+    }
+    }
+    }
+
+
